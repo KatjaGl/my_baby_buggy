315451e201aa4809c2b84b508e448b967cd4db29
Emmanuelle MAPELLI <emmanuellemapelli@gmail.com>
2022-06-06 17:33:31 +0200

    installation des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/products', to: 'products#index'
+  get '/users/new', to: 'users#new'
+  get '/products/:id', to: 'products#show'
+  get '/products/:id/locations/new', to: 'locations#new'
+  post '/products/:id/locations', to: 'locations#create'
 end

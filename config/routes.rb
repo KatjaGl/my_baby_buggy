50a897253f0c8d329bb69d85373ebe0699016388
jas <yasmine.derrag@outlook.fr>
2022-06-08 16:10:24 +0200

    homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  root to: 'pages#home'
   devise_for :users
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :products, only: [:index,:show, :new, :create, :destroy, :edit, :update]
+  resources :products, only: [:index,:show, :new, :create, :destroy, :edit, :update] do
+  resources :locations, only:[:new , :create]
+  end
   resources :categories
-  resources :locations, only: [:index,:show, :new, :create, :destroy, :edit, :update]
+  resources :locations, only: [:index,:show , :destroy, :edit, :update]
   resources :users
   # get '/products', to: 'products#index'
   # get '/users/new', to: 'users#new'

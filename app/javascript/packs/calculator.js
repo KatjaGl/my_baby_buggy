59a32cb4d437468699eacafa92b15e63525389a9
Anne-sophie Monville <annesophiemonville@gmail.com>
2022-06-09 17:21:05 +0200

    j'ai modif la page show

---
+++ app/javascript/packs/calculator.js
@@ -0,0 +1,14 @@
+
+const calculator = () => {
+  // when bottom of window hit bottom of div backgroud go down depending on the scroll value
+  const form = document.querySelector('#inputGroupSelect01')
+  if (form) {
+    const total = document.querySelector('.total')
+    const price = form.dataset.price;
+    form.addEventListener('change', () => {
+      total.innerText = `${parseInt(form.value) * price} €`
+    })
+  }
+}
+
+export { calculator };

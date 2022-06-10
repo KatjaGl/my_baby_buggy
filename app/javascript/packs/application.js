94cfb79a1d8bc60edab7ccd691373fd42063fa50
Anne-sophie Monville <annesophiemonville@gmail.com>
2022-06-10 12:35:45 +0200

    ajout de la photo sur la show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,13 @@ import "controllers"
 import "bootstrap"
 
 
+import { btnScrollToTop } from "../plugins/buttonscroll";
+document.addEventListener('turbolinks:load', () => {
+  // Call your functions here, e.g:
+  btnScrollToTop();
+});
+
+
 document.addEventListener('turbolinks:load', () => {
   calculator();
 });
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,14 +14,14 @@
   puts "#{Category.count} categories ont été créeés"
 
 
-  prod1 = Product.create!(category_id: poussette.id, number_of_place: 1, weight: 8, age_of_product: 1, details:"en très bon état, légère et confortable, se plie facilement", child_weight: 5, marque:"cybex", color:"black", prix_par_jour:5, rating: 5)
-  prod2 = Product.create!([{ category_id: siège_auto.id, number_of_place: 1, weight: 6, age_of_product: 2, details:"en très bon état, pivotant 360", child_weight: 3, marque:"britax romer", color:"bleu", prix_par_jour:5, rating: 5}])
-  prod3 = Product.create!(category_id: poussette.id, number_of_place: 1, weight: 8, age_of_product: 1, details:"en très bon état, légère et confortable, se plie facilement", child_weight: 5, marque:"cybex", color:"black", prix_par_jour:5, rating: 5)
-  prod4 = Product.create!([{ category_id: siège_auto.id, number_of_place: 1, weight: 6, age_of_product: 2, details:"en très bon état, pivotant 360", child_weight: 3, marque:"britax romer", color:"bleu", prix_par_jour:5, rating: 5}])
-  prod5 = Product.create!(category_id: poussette.id, number_of_place: 1, weight: 8, age_of_product: 1, details:"en très bon état, légère et confortable, se plie facilement", child_weight: 5, marque:"cybex", color:"black", prix_par_jour:5, rating: 5)
-  prod6 = Product.create!([{ category_id: siège_auto.id, number_of_place: 1, weight: 6, age_of_product: 2, details:"en très bon état, pivotant 360", child_weight: 3, marque:"britax romer", color:"bleu", prix_par_jour:5, rating: 5}])
-  prod7 = Product.create!(category_id: poussette.id, number_of_place: 1, weight: 8, age_of_product: 1, details:"en très bon état, légère et confortable, se plie facilement", child_weight: 5, marque:"cybex", color:"black", prix_par_jour:5, rating: 5)
-  prod8 = Product.create!([{ category_id: siège_auto.id, number_of_place: 1, weight: 6, age_of_product: 2, details:"en très bon état, pivotant 360", child_weight: 3, marque:"britax romer", color:"bleu", prix_par_jour:5, rating: 5}])
+  prod1 = Product.create!(category_id: poussette.id, product_photo:"loca.jpeg", city: "Nice", number_of_place: 1, weight: 8, age_of_product: 1, details:"en très bon état, légère et confortable, se plie facilement", child_weight: 5, marque:"cybex", color:"black", prix_par_jour:5, rating: 5)
+  prod2 = Product.create!([{ category_id: siège_auto.id, product_photo: "pouss.jpeg", number_of_place: 1, city: "Nice", weight: 6, age_of_product: 2, details:"en très bon état, pivotant 360", child_weight: 3, marque:"britax romer", color:"bleu", prix_par_jour:5, rating: 5}])
+  prod3 = Product.create!(category_id: poussette.id, product_photo:"loca1.jpeg", city: "Nice",  number_of_place: 1, weight: 8, age_of_product: 1, details:"en très bon état, légère et confortable, se plie facilement", child_weight: 5, marque:"cybex", color:"black", prix_par_jour:5, rating: 5)
+  prod4 = Product.create!([{ category_id: siège_auto.id, product_photo: "pouu.jpeg", number_of_place: 1,city: "Nice",  weight: 6, age_of_product: 2, details:"en très bon état, pivotant 360", child_weight: 3, marque:"britax romer", color:"bleu", prix_par_jour:5, rating: 5}])
+  prod5 = Product.create!(category_id: poussette.id, product_photo:"loca2.jpeg", city: "Paris",  number_of_place: 1, weight: 8, age_of_product: 1, details:"en très bon état, légère et confortable, se plie facilement", child_weight: 5, marque:"cybex", color:"black", prix_par_jour:5, rating: 5)
+  prod6 = Product.create!([{ category_id: siège_auto.id, product_photo: "loca3.jpeg", number_of_place: 1,city: "Paris",  weight: 6, age_of_product: 2, details:"en très bon état, pivotant 360", child_weight: 3, marque:"britax romer", color:"bleu", prix_par_jour:5, rating: 5}])
+  prod7 = Product.create!(category_id: poussette.id, product_photo:"loca.jpeg", city: "Paris", number_of_place: 1, weight: 8, age_of_product: 1, details:"en très bon état, légère et confortable, se plie facilement", child_weight: 5, marque:"cybex", color:"black", prix_par_jour:5, rating: 5)
+  prod8 = Product.create!([{ category_id: siège_auto.id, product_photo: "loca1.jpeg", number_of_place: 1, city: "Paris", weight: 6, age_of_product: 2, details:"en très bon état, pivotant 360", child_weight: 3, marque:"britax romer", color:"bleu", prix_par_jour:5, rating: 5}])
   puts "#{Product.count} produits ont été créeés"
 
   user = User.create(email: "test@live.fr", password: "azerty")
